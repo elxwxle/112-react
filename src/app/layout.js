0f4f2c81ebba4ac2a3b7572c9a95446fd177dbc7
elxwxle <136634687+elxwxle@users.noreply.github.com>
2024-06-25 18:16:48 +0800

    Initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Flowbite } from "flowbite-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Flowbite>
+        {children}
+      </Flowbite>
     </html>
   );
 }
